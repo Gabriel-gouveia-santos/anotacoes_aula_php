132fadfe6889b3737eda3215edd89f7326aac5b4
Jeferson Ferreira <jeferson.oferreira@etec.sp.gov.br>
2023-08-21 17:59:22 -0300

    alterações

--- calc_falta.php
+++ calc_falta.php
@@ -3,11 +3,20 @@ include "cabecalho.php";
 ?>
 Calculando....
 <?php
-$total_aula = $_POST['total_aula'];
-$total_falta = $_POST['total_falta'];
-$resultado = $total_aula - $total_falta;
+    $total_aula = $_POST['total_aula'];
+    $total_falta = $_POST['total_falta'];
+    $resultado = $total_aula - $total_falta;
+    if($total_falta>$total_aula){
+        echo "nao é possivel calcular."; 
 ?>
+<a href="">Voltar</a>
+
+<?php 
+    } else { 
+?>
+
 <button class="btn btn-success"><?php echo $resultado;?></button>
 <?php 
+}
 include "rodape.php";
 ?>
